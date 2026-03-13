bbd9aef9f7d4ae89e2b5b7ff8cf42783e7ac1cc8
Siyo Nonganga <siyosouth@gmail.com>
2026-03-13 03:00:05 +0200

    Added animated stars

--- cosmicheist/assets/js/app.js
+++ cosmicheist/assets/js/app.js
@@ -8,3 +8,51 @@ searchInput.addEventListener("keyup", function() {
         else post.style.display = "none";
     });
 });
+
+const canvas = document.getElementById("starfield");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let stars = [];
+
+for (let i = 0; i < 250; i++) {
+
+    stars.push({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        radius: Math.random() * 1.5,
+        speed: Math.random() * 0.3,
+
+        // colorful stars
+        color: `hsl(${Math.random()*360}, 80%, 70%)`
+    });
+
+}
+
+function animateStars(){
+
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    stars.forEach(star => {
+
+        ctx.beginPath();
+        ctx.arc(star.x, star.y, star.radius, 0, Math.PI*2);
+        ctx.fillStyle = star.color;
+        ctx.fill();
+
+        star.y += star.speed;
+
+        if(star.y > canvas.height){
+            star.y = 0;
+            star.x = Math.random()*canvas.width;
+        }
+
+    });
+
+    requestAnimationFrame(animateStars);
+
+}
+
+animateStars();
