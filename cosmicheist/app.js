52a750257d6f0c28dd6da75320b2bc2fe28f50a4
siyononganga <siyosouth@gmail.com>
2026-03-22 20:27:25 +0000

    added active feature highlighter

--- cosmicheist/app.js
+++ cosmicheist/app.js
@@ -285,3 +285,26 @@ window.addEventListener("scroll", () => {
 
   heroText.style.opacity = opacity;
 });
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".navbar a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+            const sectionHeight = section.offsetHeight;
+
+                if (scrollY >= sectionTop - sectionHeight / 3) {
+                      current = section.getAttribute("id");
+                          }
+                            });
+
+                              navLinks.forEach(link => {
+                                  link.classList.remove("active");
+                                      if (link.getAttribute("href") === "#" + current) {
+                                            link.classList.add("active");
+                                                }
+                                                  });
+                                                  });
