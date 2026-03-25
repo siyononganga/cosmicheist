c122941b27b79d6882e88b892f91d06805bdf37a
siyononganga <siyosouth@gmail.com>
2026-03-25 06:05:14 +0000

    no changes were made

--- cosmicheist/app.js
+++ cosmicheist/app.js
@@ -339,7 +339,7 @@ menuToggle.addEventListener("click", () => {
 
 
                                     // Close menu when a link is clicked (important for mobile UX)
-                                    const navItems = document.querySelectorAll(".nav-links a");
+                                    const navItems = document.querySelectorAll(".nav-links ul");
 
                                     navItems.forEach(link => {
                                         link.addEventListener("click", () => {
