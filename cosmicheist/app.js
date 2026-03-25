6a3e5dd25e5cea27d4a442739721c07a2c2dd3c1
siyononganga <siyosouth@gmail.com>
2026-03-25 19:48:43 +0000

    no changes

--- cosmicheist/app.js
+++ cosmicheist/app.js
@@ -331,7 +331,7 @@ menuToggle.addEventListener("click", () => {
         if (navContainer.classList.contains("active")) {
                 menuToggle.textContent = "🍟";
                     } else {
-                            menuToggle.textContent = "🍟";
+                            menuToggle.textContent = "🍔";
                                 }
                                 });
 
@@ -341,7 +341,7 @@ menuToggle.addEventListener("click", () => {
                                 navItems.forEach(link => {
                                     link.addEventListener("click", () => {
                                             navContainer.classList.remove("active");
-                                                    menuToggle.textContent = "🍟";
+                                                    menuToggle.textContent = "🍔";
                                                         });
                                                         });
 
@@ -353,6 +353,6 @@ menuToggle.addEventListener("click", () => {
 
                                                                                 if (!isClickInside) {
                                                                                         navContainer.classList.remove("active");
-                                                                                                menuToggle.textContent = "🍟";
+                                                                                                menuToggle.textContent = "🍔";
                                                                                                     }
                                                                                                     });
